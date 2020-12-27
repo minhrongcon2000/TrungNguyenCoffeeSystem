f3de1a52d2ef1878faf86a8f43565f52cdaf8acd
Pham Hoang Minh <minhrongcon2000@gmail.com>
2020-12-27 11:32:58 +0700

    add project boilerplate
    - add deployment, use case, and class diagram with relevant documents
    - add project boilerplate (ReactJS)

---
+++ project/view/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MediaCard from './MediaCard';
+
+function App() {
+  return (
+    <div className="App">
+      <MediaCard/>
+    </div>
+  );
+}
+
+export default App;
